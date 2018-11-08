fad1e6b9dba5991e3e32a1a701292fa7117a2ea8
Alexander Plotnikov <alexander-pl@mail.ru>
2018-11-08 17:13:25 +0300

    Update create-reddit-vm.sh

--- config-scripts/create-reddit-vm.sh
+++ config-scripts/create-reddit-vm.sh
@@ -2,7 +2,7 @@
 
 # Start VM 
 gcloud compute instances create reddit-app\
-  --boot-disk-size=11GB \
+  --boot-disk-size=12GB \
   --image-family=reddit-full \
   --machine-type=g1-small \
   --tags puma-server \
