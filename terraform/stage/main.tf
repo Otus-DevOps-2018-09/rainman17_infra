bb2cffc49aa36ff9be0c95075cc70a2cdbc2b97f
Alexander Plotnikov <alexander-pl@mail.ru>
2018-11-21 16:11:18 +0300

    Some refreshment3

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -9,6 +9,7 @@ module "app" {
   public_key_path = "${var.public_key_path}"
   zone            = "${var.zone}"
   app_disk_image  = "${var.app_disk_image}"
+  private_key_path = "${var.private_key_path}"
 }
 
 module "db" {
@@ -16,6 +17,7 @@ module "db" {
   public_key_path = "${var.public_key_path}"
   zone            = "${var.zone}"
   db_disk_image   = "${var.db_disk_image}"
+  private_key_path = "${var.private_key_path}"
 }
 
 module "vpc" {
